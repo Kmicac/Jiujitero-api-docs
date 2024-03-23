db31aa177fc56941e3d83a05cc087ffe47682f20
Camilo <camilojpg30@gmail.com>
2024-03-23 05:49:16 -0300

    getting started

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,23 +5,53 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
+			title: 'Jiu Jitero Api Place',
 			social: {
-				github: 'https://github.com/withastro/starlight',
+				github: 'https://github.com/Kmicac/Jiujitero-Api',
 			},
 			sidebar: [
 				{
-					label: 'Guides',
+					label: 'About',
 					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', link: '/guides/example/' },
+						{ label: 'Introduction', link: '/guides/introduction.md/' },
 					],
 				},
 				{
-					label: 'Reference',
+					label: 'Rest API',
 					autogenerate: { directory: 'reference' },
+					items: [
+						{ label: 'Academys', link: ''},
+						{ label: 'Athletes', link: ''},
+						{ label: 'Filter athletes', link: ''},
+						{ label: 'Users', link: ''},
+						{ label: 'Auth JWT', link: ''},
+						{ label: 'Swagger Docs', link: ''}
+					],
+				},
+				{
+					label: 'GraphQL',
+					items: [
+						{ label: 'Academys', link: ''},
+						{ label: 'Athletes', link: ''},
+						{ label: 'Filter athletes', link: ''},
+						{ label: 'Users', link: ''},
+						{ label: 'Auth JWT', link: ''},
+						{ label: 'Playground', link: ''}
+					],
+
+				},
+				{
+					label: 'Resources',
+					items: [
+						{label: 'Insomnia', link: ''},
+					],
 				},
 			],
+			customCss: [
+				"@fontsource/ibm-plex-serif/400.css",
+				"@fontsource/ibm-plex-serif/600.css",
+				"./src/styles/custom.css",
+			  ],
 		}),
 	],
 });
