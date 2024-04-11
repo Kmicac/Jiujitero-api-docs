252a137f958978eb05bb911363f02d981da81ba5
Camilo <camilojpg30@gmail.com>
2024-04-11 17:15:04 -0300

    fixed http

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: vercel(),
   integrations: [starlight({
     title: 'Jiu Jitero Api',
     logo: {
