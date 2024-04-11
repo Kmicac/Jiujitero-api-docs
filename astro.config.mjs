ff73140aec0702a9adc3506d9c18fe1ab4ae74e5
Camilo <camilojpg30@gmail.com>
2024-04-11 17:06:54 -0300

    add vercel config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,59 +1,75 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'Jiu Jitero Api',
-			logo: {
-				src: "./src/assets/logo.png",
-			  },
-			social: {
-				github: 'https://github.com/Kmicac/Jiujitero-Api',
-			},
-			sidebar: [
-				{
-					label: 'About',
-					items: [
-						{ label: 'Introduction', link: 'en/about/introduction/' },
-					],
-				},
-				{
-					label: 'Rest API',
-					items: [
-						{ label: 'Academys', link: 'en/rest/academy' },
-						{ label: 'Athletes', link: 'en/rest/athlete' },
-						{ label: 'Filter athletes', link: 'en/rest/filterathlete' },
-						{ label: 'Users', link: 'en/rest/user' },
-						{ label: 'Auth JWT', link: 'en/rest/auth' },
-						{ label: 'Swagger Docs', link: 'en/rest/swagger' }
-					],
-				},
-				{
-					label: 'GraphQL',
-					items: [
-						{ label: 'Academys', link: 'en/gralphql/academy' },
-						{ label: 'Athletes', link: 'en/gralphql/athlete' },
-						{ label: 'Filter athletes', link: 'en/gralphql/filter' },
-						{ label: 'Users', link: 'en/gralphql/users' },
-						{ label: 'Auth JWT', link: 'en/gralphql/auth' },
-						{ label: 'Playground', link: 'en/gralphql/playground' }
-					],
-
-				},
-				{
-					label: 'Resources',
-					items: [
-						{ label: 'Insomnia', link: 'en/about/resourse/insomnia' },
-					],
-				},
-			],
-			customCss: [
-				"@fontsource/ibm-plex-serif/400.css",
-				"@fontsource/ibm-plex-serif/600.css",
-				"./src/styles/custom.css",
-			],
-		}),
-	],
+  integrations: [starlight({
+    title: 'Jiu Jitero Api',
+    logo: {
+      src: "./src/assets/logo.png"
+    },
+    social: {
+      github: 'https://github.com/Kmicac/Jiujitero-Api'
+    },
+    sidebar: [{
+      label: 'About',
+      items: [{
+        label: 'Introduction',
+        link: 'en/about/introduction/'
+      }]
+    }, {
+      label: 'Rest API',
+      items: [{
+        label: 'Academys',
+        link: 'en/rest/academy'
+      }, {
+        label: 'Athletes',
+        link: 'en/rest/athlete'
+      }, {
+        label: 'Filter athletes',
+        link: 'en/rest/filterathlete'
+      }, {
+        label: 'Users',
+        link: 'en/rest/user'
+      }, {
+        label: 'Auth JWT',
+        link: 'en/rest/auth'
+      }, {
+        label: 'Swagger Docs',
+        link: 'en/rest/swagger'
+      }]
+    }, {
+      label: 'GraphQL',
+      items: [{
+        label: 'Academys',
+        link: 'en/gralphql/academy'
+      }, {
+        label: 'Athletes',
+        link: 'en/gralphql/athlete'
+      }, {
+        label: 'Filter athletes',
+        link: 'en/gralphql/filter'
+      }, {
+        label: 'Users',
+        link: 'en/gralphql/users'
+      }, {
+        label: 'Auth JWT',
+        link: 'en/gralphql/auth'
+      }, {
+        label: 'Playground',
+        link: 'en/gralphql/playground'
+      }]
+    }, {
+      label: 'Resources',
+      items: [{
+        label: 'Insomnia',
+        link: 'en/about/resourse/insomnia'
+      }]
+    }],
+    customCss: ["@fontsource/ibm-plex-serif/400.css", "@fontsource/ibm-plex-serif/600.css", "./src/styles/custom.css"]
+  })],
+  output: "server",
+  adapter: vercel()
 });
